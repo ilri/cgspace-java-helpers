edd08c859a5c7b1befd8a8f7ba5a00e0d0c75f9b
Alan Orth <alan.orth@gmail.com>
2020-07-31 23:37:06 +0300

    CountryCodeTagger.java: Remove FileReader import
    We are using an InputStream now.

--- src/main/java/org/cgiar/cgspace/ctasks/CountryCodeTagger.java
+++ src/main/java/org/cgiar/cgspace/ctasks/CountryCodeTagger.java
@@ -28,7 +28,6 @@ import org.dspace.curate.AbstractCurationTask;
 import org.dspace.curate.Curator;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
