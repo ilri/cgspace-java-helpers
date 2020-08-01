4a6edba4672ece9a286dcebcc119cfd833376d8f
Alan Orth <alan.orth@gmail.com>
2020-08-01 11:49:22 +0300

    src/main/java: Add cgspace_name to Countries class
    We will eventually use this to read CGSpace-specific mappings to
    ISO 3166-1 values.

--- src/main/java/org/cgiar/cgspace/ctasks/CountriesVocabulary.java
+++ src/main/java/org/cgiar/cgspace/ctasks/CountriesVocabulary.java
@@ -30,6 +30,7 @@ public class CountriesVocabulary {
         private String name;            //required
         private String common_name;     //optional
         private String official_name;   //optional
+        private String cgspace_name;    //optional
         private String numeric;         //required Hmmmm need to cast this...
         private String alpha_2;         //required
         private String alpha_3;         //required
@@ -37,12 +38,14 @@ public class CountriesVocabulary {
         public Country(String name,
                        @Nullable String common_name,
                        @Nullable String official_name,
+                       @Nullable String cgspace_name,
                        String numeric,
                        String alpha_2,
                        String alpha_3) {
             this.name = name;
             this.common_name = common_name;
             this.official_name = official_name;
+            this.cgspace_name = cgspace_name;
             this.numeric = numeric; // fuuuuu this is a string and we can't cast to Integer because some values are zeropadded like "004"
             this.alpha_2 = alpha_2;
             this.alpha_3 = alpha_3;
@@ -70,6 +73,10 @@ public class CountriesVocabulary {
 
         public String getAlpha_3() {
             return alpha_3;
+        }
+
+        public String getCgspace_name() {
+            return cgspace_name;
         }
     }
 }
